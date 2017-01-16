e59801feaf6495541a71aabe4d8e46e805629e5c
Ravi <Ravi@10.1.10.178>
2017-01-16 15:12:15 -0800

    Context and Init(Servlet Config) parameters

--- src/org/ravi/javabrains/SimpleServlet.java
+++ src/org/ravi/javabrains/SimpleServlet.java
@@ -3,7 +3,9 @@ package org.ravi.javabrains;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,7 +15,9 @@ import javax.servlet.http.HttpSession;
 /**
  * Servlet implementation class SimpleServlet
  */
-@WebServlet(description = "A simple servlet description", urlPatterns = { "/SimpleServletPath" })
+@WebServlet(description = "A simple servlet description", urlPatterns = { "/SimpleServletPath" },
+		initParams = {@WebInitParam(name="defaultUser", value = "guest")}
+		)
 public class SimpleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -32,12 +36,16 @@ public class SimpleServlet extends HttpServlet {
 		response.setContentType("text/html");
 		String userName = request.getParameter("username");
 		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
 		if (userName != "" && userName != null){
 			session.setAttribute("savedUserName", userName);
+			context.setAttribute("savedUserName", userName);
 		}
 		PrintWriter pWriter = response.getWriter();
-		pWriter.println("Request parameter has username : " + userName);
-		pWriter.println("Session parameter has username : " + (String)session.getAttribute("savedUserName"));
+		pWriter.println("<br>Request parameter has username : " + userName);
+		pWriter.println("<br>Session parameter has username : " + (String)session.getAttribute("savedUserName"));
+		pWriter.println("<br>Context parameter has username : " + (String)context.getAttribute("savedUserName"));
+		pWriter.println("<br>Init value : " + this.getServletConfig().getInitParameter("defaultUser"));
 		
 	}
 
